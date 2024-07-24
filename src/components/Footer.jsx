524f08b40371e5b229451f5f8360d1a4cf79d041
massimo altamura <maltamura494@gmail.com>
2024-07-24 11:00:14 +0200

    aggiunto freccie e backgraoud

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,3 +1,4 @@
+/*Component author Massimo*/
 import React, { useState } from 'react';
 import './Footer.scss';
 import { Link } from 'react-router-dom';
@@ -8,11 +9,15 @@ import tiktok from '../images/tiktok.png';
 import twitter from '../images/twitter.png';
 import whatsapp from '../images/whatsapp.png';
 import youtube from '../images/youtube.png';
-import logo from '../images/logo.png'
+import logo from '../images/logo.png';
+import { MetalBg } from "./MetalBg";
+import { NavbarBorder } from './NavbarBorder';
+
 const Footer = () => {
   const [activeSection, setActiveSection] = useState('');
   const isMobile = useMediaQuery({ query: '(max-width: 320px)' });
   const iMobile = useMediaQuery({ query: '(max-width: 768px)' });
+
   const toggleSection = (section) => {
     setActiveSection(activeSection === section ? '' : section);
   };
@@ -22,86 +27,100 @@ const Footer = () => {
     { to: '/terms', text: 'Termini di Servizio' },
     { to: '/mail', text: 'Mail' }
   ];
-  const gamingPc=[
-    {to: "/", text: 'Gaming Portatile'},
-    {to: "/", text: 'Gaming Fisso'},
-    {to: "/", text: 'PC Gaming i9'},
-    {to: "/", text: 'PC Gaming i7'},
-    {to: "/", text: 'Workstation Grafiche'},
-  ]
+  const gamingPc = [
+    { to: "/", text: 'Gaming Portatile' },
+    { to: "/", text: 'Gaming Fisso' },
+    { to: "/", text: 'PC Gaming i9' },
+    { to: "/", text: 'PC Gaming i7' },
+    { to: "/", text: 'Workstation Grafiche' },
+  ];
   const footerSection = [
-    {to: "/", text: 'Server per Aziende'},
-    {to: "/", text: 'Server per Studio'},
-    {to: "/", text: 'Professionale PC'},
-    {to: "/", text: '3D Rendering PC'},
-    {to: "/", text: 'Workstation Grafiche'},
-  ]
+    { to: "/", text: 'Server per Aziende' },
+    { to: "/", text: 'Server per Studio' },
+    { to: "/", text: 'Professionale PC' },
+    { to: "/", text: '3D Rendering PC' },
+    { to: "/", text: 'Workstation Grafiche' },
+  ];
+
   return (
-    <footer className="relative footer">
-      <div className="footer-section company-info">
-        <img src={logo} alt="Company Logo" className="company-logo" />
-        <address>
-          Società: Classe Fullstack27 s.p.a<br />
-          Sede: Via Santa Maria Delle Grazie 183, Nocera Superiore 84015<br />
-          Partita IVA: IT0899XXXXX<br />
-          Tel: 3420541376
-        </address>
+    <>
+      <footer className="relative footer">
+        <div className='min-h-full py-px bg-gradient box-border w-full'>
+          <div className='absolute top-0 left-0 w-full rotate-180'>
+            <NavbarBorder />
+          </div>
 
-        <div className="flex social-icons">
-          <a href="#"><img src={facebook} alt="Facebook" /></a>
-          <a href="#"><img src={instragram} alt="Instagram" /></a>
-          <a href="#"><img src={twitter} alt="Twitter" /></a>
-          <a href="#"><img src={youtube} alt="Youtube" /></a>
-          <a href="#"><img src={whatsapp} alt="Whatsapp" /></a>
-          <a href="#"><img src={tiktok} alt="TikTok" /></a>
+          <MetalBg>
+            <div className='flex footer-contect'>
+              <div className="footer-section company-info">
+                <img src={logo} alt="Company Logo" className="company-logo" />
+                <address>
+                  Società: Classe Fullstack27 s.p.a<br />
+                  Sede: Via Santa Maria Delle Grazie 183, Nocera Superiore 84015<br />
+                  Partita IVA: IT0899XXXXX<br />
+                  Tel: 3420541376
+                </address>
+                <div className="flex social-icons">
+                  <a href="#"><img src={facebook} alt="Facebook" /></a>
+                  <a href="#"><img src={instragram} alt="Instagram" /></a>
+                  <a href="#"><img src={twitter} alt="Twitter" /></a>
+                  <a href="#"><img src={youtube} alt="Youtube" /></a>
+                  <a href="#"><img src={whatsapp} alt="Whatsapp" /></a>
+                  <a href="#"><img src={tiktok} alt="TikTok" /></a>
+                </div>
+              </div>
+              <div className="footer-section links">
+                <h4 onClick={() => toggleSection('gaming')} className="toggle-button">
+                  GAMING PC
+                  <span className={`arrow ${activeSection === 'gaming' ? 'active' : ''}`}>&#9660;</span>
+                </h4>
+                {(!isMobile || activeSection === 'gaming') && (!iMobile || activeSection === 'gaming') && (
+                  <ul>
+                    {gamingPc.map((link, index) => (
+                      <li key={index}>
+                        <Link to={link.to}>{link.text}</Link>
+                      </li>
+                    ))}
+                  </ul>
+                )}
+              </div>
+              <div className="footer-section links">
+              <h4 onClick={() => toggleSection('professional')} className="toggle-button">
+                  PROFESSIONAL PC
+                  <span className={`arrow ${activeSection === 'professional' ? 'active' : ''}`}>&#9660;</span>
+                </h4>
+                {(!isMobile || activeSection === 'professional') && (!iMobile || activeSection === 'professional') && (
+                  <ul>
+                    {footerSection.map((link, index) => (
+                      <li key={index}>
+                        <Link to={link.to}>{link.text}</Link>
+                      </li>
+                    ))}
+                  </ul>
+                )}
+              </div>
+              <div className="footer-section misc-links">
+                <ul>
+                  {footerLinks.map((link, index) => (
+                    <li key={index}>
+                      <Link to={link.to}>{link.text}</Link>
+                    </li>
+                  ))}
+                </ul>
+              </div>
+              <div className="footer-section company-description">
+                <p>
+                  Classe Fullstack27 s.p.a è una società specializzata nella distribuzione e vendita di componenti di pc e vendita di computer
+                </p>
+                <p>
+                  Fondata nel 2024, Classe Fullstack27 s.p.a è una società di ragazzi
+                </p>
+              </div>
+            </div>
+          </MetalBg>
         </div>
-      </div>
-
-      <div className="footer-section links">
-        <h4 onClick={() => toggleSection('gaming')} className="toggle-button">GAMING PC</h4>
-        {(!isMobile || activeSection === 'gaming') && (!iMobile || activeSection === 'gaming') &&(
-          <ul>
-            {gamingPc.map((link,index)=>(
-              <li key={index}>
-                <Link to={link.to}>{link.text}</Link>
-              </li>
-            ))}
-          </ul>
-        )}
-      </div>
-
-      <div className="footer-section links">
-        <h4 onClick={() => toggleSection('professional')} className="toggle-button">PROFESSIONAL PC</h4>
-        {(!isMobile || activeSection === 'professional') && (!iMobile || activeSection === 'professional') &&(
-          <ul>
-           {footerSection.map((link,index)=>(
-              <li key={index}>
-                <Link to={link.to}>{link.text}</Link>
-              </li>
-            ))}
-          </ul>
-        )}
-      </div>
-
-      <div className="footer-section misc-links">
-        <ul>
-          {footerLinks.map((link, index) => (
-            <li key={index}>
-              <Link to={link.to}>{link.text}</Link>
-            </li>
-          ))}
-        </ul>
-      </div>
-
-      <div className="footer-section company-description">
-        <p>
-          Classe Fullstack27 s.p.a è una società specializzata nella distribuzione e vendita di componenti di pc e vendita di computer
-        </p>
-        <p>
-          Fondata nel 2024, Classe Fullstack27 s.p.a è una società di ragazzi
-        </p>
-      </div>
-    </footer>
+      </footer>
+    </>
   );
 };
 
