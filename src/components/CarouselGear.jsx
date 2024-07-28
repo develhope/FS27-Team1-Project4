2908c7f07b1d78a5f01915042f1de5a08a81da52
DomenicoProvenzano13 <domenicoprovenzano13@yahoo.it>
2024-07-28 22:40:11 +0200

    carosello in progress

--- src/components/CarouselGear.jsx
+++ src/components/CarouselGear.jsx
@@ -13,10 +13,7 @@ export function CarouselGear() {
   return (
     <div className="gear-slide">
       <div className="gear-slide-images">
-        {images.map((src, index) => (
-          <img key={index} src={src} alt={`Slide ${index}`} />
-        ))}
-        {images.map((src, index) => (
+        {images.concat(images).map((src, index) => (
           <img key={index} src={src} alt={`Slide ${index}`} />
         ))}
       </div>
