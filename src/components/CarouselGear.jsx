4ca4a2498cd624f973eef18eb79787a165ca6c2b
DomenicoProvenzano13 <domenicoprovenzano13@yahoo.it>
2024-08-05 17:07:34 +0200

    creazione card

--- src/components/CarouselGear.jsx
+++ src/components/CarouselGear.jsx
@@ -9,14 +9,74 @@ const images = [
   "/src/assets/domy-hacker.png",
 ];
 
+const cards = [
+  {
+    id: 1,
+    title: "card1",
+    sub_title: "sub title 1",
+    image: "/src/assets/h.webp",
+  },
+  {
+    id: 2,
+    title: "card2",
+    sub_title: "sub title 2",
+    image: "/src/assets/h.webp",
+  },
+  {
+    id: 3,
+    title: "card3",
+    sub_title: "sub title 3",
+    image: "/src/assets/h.webp",
+  },
+  {
+    id: 4,
+    title: "card4",
+    sub_title: "sub title 4",
+    image: "/src/assets/h.webp",
+  },
+  {
+    id: 5,
+    title: "card5",
+    sub_title: "sub title 5",
+    image: "/src/assets/h.webp",
+  },
+  {
+    id: 6,
+    title: "card6",
+    sub_title: "sub title 6",
+    image: "/src/assets/h.webp",
+  },
+  {
+    id: 7,
+    title: "card7",
+    sub_title: "sub title 7",
+    image: "/src/assets/h.webp",
+  },
+  {
+    id: 8,
+    title: "card8",
+    sub_title: "sub title 8",
+    image: "/src/assets/h.webp",
+  },
+];
+
 export function CarouselGear() {
   return (
-    <div className="gear-slide">
-      <div className="gear-slide-images">
-        {images.concat(images).map((src, index) => (
-          <img key={index} src={src} alt={`Slide ${index}`} />
-        ))}
+    <>
+      <div className="gear-slide">
+        <div className="gear-slide-images">
+          {images.concat(images).map((src, index) => (
+            <img key={index} src={src} alt={`Slide ${index}`} />
+          ))}
+        </div>
       </div>
-    </div>
+      <div className="gear-slide">
+        <div className="gear-slide-images">
+          {cards.concat(cards).map((src, index) => (
+            <img key={index} src={src} alt={`Slide ${index}`} />
+          ))}
+        </div>
+      </div>
+    </>
   );
 }
