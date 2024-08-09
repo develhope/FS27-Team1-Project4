c02edbadbcf96c37a5bf93e50dc0753f1f564481
DomenicoProvenzano13 <domenicoprovenzano13@yahoo.it>
2024-08-09 16:20:36 +0200

    creata struttura semicerchio con reticolo icone

--- src/components/SiteGuide.jsx
+++ src/components/SiteGuide.jsx
@@ -2,7 +2,16 @@ export function SiteGuide() {
   return (
     <div className="banner-guide">
       <div className="semi-circle"></div>
-      <div className="steps"></div>
+      <div className="steps">
+        <div className="steps-1st-row">
+          <div className="step"></div>
+          <div className="step"></div>
+        </div>
+        <div className="steps-2nd-row">
+          <div className="step"></div>
+          <div className="step"></div>
+        </div>
+      </div>
     </div>
   );
 }
