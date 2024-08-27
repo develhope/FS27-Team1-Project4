7ac75be3168ad0392cf857087f6f3530e5b13c96
DomenicoProvenzano13 <domenicoprovenzano13@yahoo.it>
2024-08-27 17:15:01 +0200

    Filtered product from carousel + media query completed

--- src/components/CarouselGear.jsx
+++ src/components/CarouselGear.jsx
@@ -1,53 +1,70 @@
+import { useNavigate } from "react-router-dom";
+
 const cards = [
   {
     id: 1,
     title: "CPU",
     image: "http://localhost:3000/uploads/intel_core_i9-13900KF.png",
+    url: "CPU",
   },
   {
     id: 2,
     title: "GPU",
     image: "http://localhost:3000/uploads/geForce_rtx_4070_ti.png",
+    url: "GPU",
   },
   {
     id: 3,
     title: "RAM",
     image: "http://localhost:3000/uploads/ram-vengeance.png",
+    url: "RAM",
   },
   {
     id: 4,
     title: "SSD",
     image: "http://localhost:3000/uploads/lexar-2tb.png",
+    url: "SSD",
   },
   {
     id: 5,
     title: "MONITOR",
     image: "http://localhost:3000/uploads/samsung-viewfinity.png",
+    url: "Monitor",
   },
   {
     id: 6,
     title: "MOUSE",
     image: "http://localhost:3000/uploads/logitech-g502.png",
+    url: "Mouse",
   },
   {
     id: 7,
     title: "KEYBOARD",
     image: "http://localhost:3000/uploads/corsair-k70.png",
+    url: "Keyboard",
   },
   {
     id: 8,
     title: "HEADSET",
     image: "http://localhost:3000/uploads/corsair-hs65.png",
+    url: "Headset",
   },
 ];
 
 export function CarouselGear() {
+  const navigate = useNavigate();
+
+  function handleCategorySelected(category) {
+    navigate(`/gear/${category}`);
+  }
+
   return (
     <div className="gear-slider">
       <div className="gear-slide-cards">
         {cards.concat(cards).map((card, index) => (
           <div
             key={index}
+            onClick={() => handleCategorySelected(card.url)}
             className={`cardGear ${
               index % 2 === 0
                 ? "cardGear-border-color1"
--- src/components/GearCollection.jsx
+++ src/components/GearCollection.jsx
@@ -1,88 +1,84 @@
 import React, { useState } from "react";
 import ProductCard from "./ProductCard";
 
-
 const GearCollection = ({ products }) => {
-    
-    const [currentPage, setCurrentPage] = useState(1);
-    const [selectedProduct, setSelectedProduct] = useState(null);
-    const itemsPerPage = 6;
- 
-    const totalPages = Math.ceil(products.length / itemsPerPage);
-    const startIndex = (currentPage - 1) * itemsPerPage;
-    const currentProducts = products.slice(startIndex, startIndex + itemsPerPage);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [selectedProduct, setSelectedProduct] = useState(null);
+  const itemsPerPage = 6;
 
-    const handlePageChange = (pageNumber) => {
-        setCurrentPage(pageNumber);
-    };
+  const totalPages = Math.ceil(products.length / itemsPerPage);
+  const startIndex = (currentPage - 1) * itemsPerPage;
+  const currentProducts = products.slice(startIndex, startIndex + itemsPerPage);
 
-    const handleNextPage = () => {
-        if (currentPage < totalPages) {
-            setCurrentPage(currentPage + 1);
-        }
-    };
+  const handlePageChange = (pageNumber) => {
+    setCurrentPage(pageNumber);
+  };
 
-    const handlePreviousPage = () => {
-        if (currentPage > 1) {
-            setCurrentPage(currentPage - 1);
-        }
-    };
+  const handleNextPage = () => {
+    if (currentPage < totalPages) {
+      setCurrentPage(currentPage + 1);
+    }
+  };
 
-    const handleProductClick = (product) => {
-        setSelectedProduct(product);
-    };
+  const handlePreviousPage = () => {
+    if (currentPage > 1) {
+      setCurrentPage(currentPage - 1);
+    }
+  };
 
-    const handleCloseDetail = () => {
-        setSelectedProduct(null);
-    };
+  const handleProductClick = (product) => {
+    setSelectedProduct(product);
+  };
 
-    return (
-        <div className="gear-collection-page">
-             (
-                <>
-                    
-                    <div className="gear-list">
-                        {currentProducts.map((product) => (
-                            <ProductCard
-                                key={product.id}
-                                name={product.gear}
-                                description={product.features.join(", ")}
-                                originalPrice={product.originalPrice}
-                                discount={product.discount}
-                                imageUrl={product.image}
-                                onClick={() => handleProductClick(product)}
-                            />
-                        ))}
-                    </div>
-                    <div className="pagination">
-                        <button
-                            className="pagination-button"
-                            onClick={handlePreviousPage}
-                            disabled={currentPage === 1}
-                        >
-                            &lt;
-                        </button>
-                        {Array.from({ length: totalPages }, (_, index) => (
-                            <button
-                                key={index + 1}
-                                className={`pagination-button ${currentPage === index + 1 ? 'active' : ''}`}
-                                onClick={() => handlePageChange(index + 1)}
-                            >
-                                {index + 1}
-                            </button>
-                        ))}
-                        <button
-                            className="pagination-button"
-                            onClick={handleNextPage}
-                            disabled={currentPage === totalPages}
-                        >
-                            &gt;
-                        </button>
-                    </div>
-                </>
-            )
-        </div>
-    );
+  const handleCloseDetail = () => {
+    setSelectedProduct(null);
+  };
+
+  return (
+    <div className="product-page-filter relative">
+      <h1 className="product-title-filter">{products[0].gear}</h1>
+      <div className="product-list-filter">
+        {currentProducts.map((product) => (
+          <ProductCard
+            key={product.id}
+            name={product.gear}
+            description={product.features.join(", ")}
+            originalPrice={product.originalPrice}
+            discount={product.discount}
+            imageUrl={product.image}
+            onClick={() => handleProductClick(product)}
+          />
+        ))}
+      </div>
+      <div className="pagination">
+        <button
+          className="pagination-button"
+          onClick={handlePreviousPage}
+          disabled={currentPage === 1}
+        >
+          &lt;
+        </button>
+        {Array.from({ length: totalPages }, (_, index) => (
+          <button
+            key={index + 1}
+            className={`pagination-button ${
+              currentPage === index + 1 ? "active" : ""
+            }`}
+            onClick={() => handlePageChange(index + 1)}
+          >
+            {index + 1}
+          </button>
+        ))}
+        <button
+          className="pagination-button"
+          onClick={handleNextPage}
+          disabled={currentPage === totalPages}
+        >
+          &gt;
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default GearCollection;
